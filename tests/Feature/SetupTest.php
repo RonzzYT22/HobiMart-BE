f931b9fa4600c58867e3aee64320139a5144f82e
Ronzz YT <ronzzyt22@gmail.com>
2026-08-16 16:10:52 +0700

    fix: CORS tests after origin change

--- tests/Feature/SetupTest.php
+++ tests/Feature/SetupTest.php
@@ -8,10 +8,12 @@ class SetupTest extends TestCase
 {
     public function test_cors_allows_frontend_origin(): void
     {
-        $response = $this->options('/api/test-cors');
+        $response = $this->options('/api/test-cors', [], [
+            'Origin' => 'http://localhost:3000',
+        ]);
 
         $response->assertStatus(200)
-            ->assertHeader('Access-Control-Allow-Origin', '*');
+            ->assertHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     }
 
     public function test_reverb_config_exists(): void
@@ -27,16 +29,17 @@ class SetupTest extends TestCase
     public function test_cors_allowed_headers(): void
     {
         $response = $this->options('/api/test-cors', [], [
+            'Origin' => 'http://localhost:3000',
             'Access-Control-Request-Method' => 'GET',
             'Access-Control-Request-Headers' => 'Content-Type, Authorization, X-Request-ID',
         ]);
 
-        $response->assertHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Request-ID');
+        $response->assertHeader('Access-Control-Allow-Headers', 'content-type, authorization, x-request-id, accept');
     }
 
     public function test_cors_exposed_headers(): void
     {
-        $response = $this->options('/api/test-cors');
+        $response = $this->options('/api/test-cors', [], ['Origin' => 'http://localhost:3000']);
 
         $response->assertHeader('Access-Control-Expose-Headers', 'X-Proxy-Status, X-Encrypted');
     }
@@ -44,6 +47,7 @@ class SetupTest extends TestCase
     public function test_cors_max_age(): void
     {
         $response = $this->options('/api/test-cors', [], [
+            'Origin' => 'http://localhost:3000',
             'Access-Control-Request-Method' => 'GET',
         ]);
 
